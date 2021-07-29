60a77ef86a25dd2efb649422f286913566be3eff
Salvatore Ingala <6681844+bigspider@users.noreply.github.com>
2021-07-29 16:08:18 +0200

    Split test_get_address into three separate tests for different address types

--- tests/test_get_address.py
+++ tests/test_get_address.py
@@ -6,29 +6,27 @@ from bitcoin_client.common import AddressType
 # TODO: add tests with display=True and non-standard path
 
 
-def test_get_address(cmd: BitcoinCommand):
+def test_get_address_legacy(cmd: BitcoinCommand):
     # legacy address
     addr = cmd.get_address(
-        address_type=AddressType.LEGACY,
-        bip32_path="m/44'/1'/0'/0/0",
-        display=False
+        address_type=AddressType.LEGACY, bip32_path="m/44'/1'/0'/0/0", display=False
     )
     assert addr == "mz5vLWdM1wHVGSmXUkhKVvZbJ2g4epMXSm"
 
+
+def test_get_address_wit(cmd: BitcoinCommand):
     # bech32 address
     addr = cmd.get_address(
-        address_type=AddressType.WIT,
-        bip32_path="m/84'/1'/0'/0/0",
-        display=False
+        address_type=AddressType.WIT, bip32_path="m/84'/1'/0'/0/0", display=False
     )
 
     assert addr == "tb1qzdr7s2sr0dwmkwx033r4nujzk86u0cy6fmzfjk"
 
+
+def test_get_address_sh_wit(cmd: BitcoinCommand):
     # P2SH-P2WPKH address
     addr = cmd.get_address(
-        address_type=AddressType.SH_WIT,
-        bip32_path="m/49'/1'/0'/0/0",
-        display=False
+        address_type=AddressType.SH_WIT, bip32_path="m/49'/1'/0'/0/0", display=False
     )
 
     assert addr == "2MyHkbusvLomaarGYMqyq7q9pSBYJRwWcsw"
