e6a8872e50d0757ab65209ba423790e7d2c67284
Salvatore Ingala <6681844+bigspider@users.noreply.github.com>
2021-11-03 15:21:19 +0100

    Make sure that the G_cx symbol is always defined even if USE_CXRAM_SECTION is not

--- src/cxram_stash.c
+++ src/cxram_stash.c
@@ -3,6 +3,12 @@
 #include "cxram_stash.h"
 #include "cx_ram.h"
 
+#ifndef G_cx
+// The G_cx symbol is only defined in the sdk if compiled with certain libs are included.
+// This makes sure that the symbol exists nonetheless.
+union cx_u G_cx;
+#endif
+
 #ifndef USE_CXRAM_SECTION
 
 uint8_t G_cxram_replacement_buffer[1024];
@@ -12,10 +18,6 @@ uint8_t *get_cxram_buffer() {
 }
 
 #else
-
-#ifndef G_cx
-union cx_u G_cx;
-#endif
 
 uint8_t *get_cxram_buffer() {
     return (uint8_t *) &G_cx;
