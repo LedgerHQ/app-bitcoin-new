f4d42b484c39a85b03c8e9176bc4285dcd8e7509
Salvatore Ingala <6681844+bigspider@users.noreply.github.com>
2021-11-09 14:18:56 +0100

    Fixed dashboard for altcoins

--- src/ui/menu.c
+++ src/ui/menu.c
@@ -21,18 +21,49 @@
 #include "../globals.h"
 #include "menu.h"
 
-UX_STEP_NOCB(ux_menu_ready_step, pnn, {&C_bitcoin_logo, "Bitcoin", "is ready"});
+// We have a screen with the icon and "Bitcoin is ready" for Bitcoin,
+// "Bitcoin Testnet is ready" for Bitcoin Testnet, "Application is ready" for all the altcoins
+UX_STEP_NOCB(ux_menu_ready_step_bitcoin, pnn, {&C_bitcoin_logo, "Bitcoin", "is ready"});
+UX_STEP_NOCB(ux_menu_ready_step_bitcoin_testnet,
+             pnn,
+             {&C_bitcoin_logo, "Bitcoin Testnet", "is ready"});
+UX_STEP_NOCB(ux_menu_ready_step_altcoin, nn, {"Application", "is ready"});
+
 UX_STEP_NOCB(ux_menu_version_step, bn, {"Version", APPVERSION});
 UX_STEP_CB(ux_menu_about_step, pb, ui_menu_about(), {&C_icon_certificate, "About"});
 UX_STEP_VALID(ux_menu_exit_step, pb, os_sched_exit(-1), {&C_icon_dashboard_x, "Quit"});
 
-// FLOW for the main menu:
+// FLOW for the main menu (for bitcoin):
 // #1 screen: ready
 // #2 screen: version of the app
 // #3 screen: about submenu
 // #4 screen: quit
-UX_FLOW(ux_menu_main_flow,
-        &ux_menu_ready_step,
+UX_FLOW(ux_menu_main_flow_bitcoin,
+        &ux_menu_ready_step_bitcoin,
+        &ux_menu_version_step,
+        &ux_menu_about_step,
+        &ux_menu_exit_step,
+        FLOW_LOOP);
+
+// FLOW for the main menu (for bitcoin testnet):
+// #1 screen: ready
+// #2 screen: version of the app
+// #3 screen: about submenu
+// #4 screen: quit
+UX_FLOW(ux_menu_main_flow_bitcoin_testnet,
+        &ux_menu_ready_step_bitcoin_testnet,
+        &ux_menu_version_step,
+        &ux_menu_about_step,
+        &ux_menu_exit_step,
+        FLOW_LOOP);
+
+// FLOW for the main menu (for altcoins):
+// #1 screen: ready
+// #2 screen: version of the app
+// #3 screen: about submenu
+// #4 screen: quit
+UX_FLOW(ux_menu_main_flow_altcoin,
+        &ux_menu_ready_step_altcoin,
         &ux_menu_version_step,
         &ux_menu_about_step,
         &ux_menu_exit_step,
@@ -43,7 +74,13 @@ void ui_menu_main() {
         ux_stack_push();
     }
 
-    ux_flow_init(0, ux_menu_main_flow, NULL);
+    if (G_coin_config->kind == COIN_KIND_BITCOIN) {
+        ux_flow_init(0, ux_menu_main_flow_bitcoin, NULL);
+    } else if (G_coin_config->kind == COIN_KIND_BITCOIN_TESTNET) {
+        ux_flow_init(0, ux_menu_main_flow_bitcoin_testnet, NULL);
+    } else {
+        ux_flow_init(0, ux_menu_main_flow_altcoin, NULL);
+    }
 }
 
 UX_STEP_NOCB(ux_menu_info_step, bn, {"Bitcoin App", "(c) 2021 Ledger"});
