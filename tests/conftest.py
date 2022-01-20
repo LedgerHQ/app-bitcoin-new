f4ab7c4a652a61d3568d4dfc9cc7ed24fdc4bab7
Salvatore Ingala <6681844+bigspider@users.noreply.github.com>
2022-01-20 14:01:12 +0100

    Make sure that speculos returns the right app version when running tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,3 +2,7 @@ import random
 from test_utils.fixtures import *
 
 random.seed(0)  # make sure tests are repeatable
+
+# Make sure that the native client library is used with, as speculos would otherwise
+# return a version number < 2.0.0 for the app
+os.environ['SPECULOS_APPNAME'] = f'Bitcoin Test:{get_app_version()}'
--- tests_mainnet/conftest.py
+++ tests_mainnet/conftest.py
@@ -2,6 +2,10 @@ import random
 import os
 from test_utils.fixtures import *
 
+# Make sure that the native client library is used, as speculos would otherwise
+# return a version number < 2.0.0 for the app
+os.environ['SPECULOS_APPNAME'] = f'Bitcoin:{get_app_version()}'
+
 os.environ["BITCOIN_NETWORK"] = "main"
 
 random.seed(0)  # make sure tests are repeatable
