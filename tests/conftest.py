7c6013ed79b9c77d035edb0177c5db9a4ec568c0
Salvatore Ingala <6681844+bigspider@users.noreply.github.com>
2022-03-10 16:58:43 +0100

    Added real e2e test for multisig

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,8 +1,165 @@
 import random
+import binascii
+import hashlib
+from typing import Tuple
+
 from test_utils.fixtures import *
+from test_utils.authproxy import AuthServiceProxy, JSONRPCException
+from test_utils import segwit_addr
+
+import shutil
+import subprocess
+from time import sleep
+from decimal import Decimal
+
+import bitcoin_client.ledger_bitcoin._base58 as base58
+from bitcoin_client.ledger_bitcoin.common import sha256
 
 random.seed(0)  # make sure tests are repeatable
 
 # Make sure that the native client library is used with, as speculos would otherwise
 # return a version number < 2.0.0 for the app
 os.environ['SPECULOS_APPNAME'] = f'Bitcoin Test:{get_app_version()}'
+
+
+BITCOIN_DIRNAME = os.getenv("BITCOIN_DIRNAME", ".test_bitcoin")
+
+
+rpc_url = "http://%s:%s@%s:%s" % (
+    os.getenv("BTC_RPC_USER", "user"),
+    os.getenv("BTC_RPC_PASSWORD", "passwd"),
+    os.getenv("BTC_RPC_HOST", "127.0.0.1"),
+    os.getenv("BTC_RPC_PORT", "18443")
+)
+
+utxos = list()
+btc_addr = ""
+
+
+def get_rpc() -> AuthServiceProxy:
+    return AuthServiceProxy(rpc_url)
+
+
+def get_wallet_rpc(wallet_name: str) -> AuthServiceProxy:
+    return AuthServiceProxy(f"{rpc_url}/wallet/{wallet_name}")
+
+
+def setup_node():
+    global btc_addr
+
+    # Check bitcoind is running while generating the address
+    while True:
+        rpc = get_rpc()
+        try:
+            print(rpc.createwallet(wallet_name="test_wallet", descriptors=True))
+            btc_addr = rpc.getnewaddress()
+            break
+
+        except ConnectionError as e:
+            sleep(1)
+        except JSONRPCException as e:
+            if "Loading wallet..." in str(e):
+                sleep(1)
+
+    # Mine enough blocks so coinbases are mature and we have enough funds to run everything
+    rpc.generatetoaddress(105, btc_addr)
+
+
+@pytest.fixture(scope="session")
+def run_bitcoind():
+    # Run bitcoind in a separate folder
+    os.makedirs(BITCOIN_DIRNAME, exist_ok=True)
+
+    bitcoind = os.getenv("BITCOIND", "bitcoind")
+
+    shutil.copy(os.path.join(os.path.dirname(__file__), "bitcoin.conf"), BITCOIN_DIRNAME)
+    subprocess.Popen([bitcoind, f"--datadir={BITCOIN_DIRNAME}"])
+
+    # Make sure the node is ready, and generate some initial blocks
+    setup_node()
+
+    yield
+
+    rpc = get_rpc()
+    rpc.stop()
+
+    shutil.rmtree(BITCOIN_DIRNAME)
+
+
+@pytest.fixture(scope="session")
+def rpc(run_bitcoind):
+    return get_rpc()
+
+
+@pytest.fixture(scope="session")
+def rpc_test_wallet(run_bitcoind):
+    return get_wallet_rpc("test_wallet")
+
+
+def get_utxo():
+    rpc = get_rpc()
+    global utxos
+    if not utxos:
+        utxos = rpc.listunspent()
+
+    if len(utxos) == 0:
+        raise ValueError("There are no UTXOs.")
+
+    utxo = utxos.pop(0)
+    while utxo.get("amount") < Decimal("0.00002"):
+        utxo = utxos.pop(0)
+
+    return utxo
+
+
+def seed_to_wif(seed: bytes):
+    assert len(seed) == 32
+
+    double_sha256 = sha256(sha256(b"\x80" + seed))
+    return base58.encode(b"\x80" + seed + double_sha256[:4])
+
+
+wallet_count = 0
+
+
+def get_unique_wallet_name() -> str:
+    global wallet_count
+
+    result = f"mywallet-{wallet_count}"
+
+    wallet_count += 1
+
+    return result
+
+
+def create_new_wallet() -> Tuple[str, str]:
+    """Creates a new descriptor-enabled wallet in bitcoin-core. Each new wallet has an increasing counter as
+    part of it's name in order to avoid conflicts."""
+
+    wallet_name = get_unique_wallet_name()
+
+    # TODO: derive seed from wallet_count, and use it to create a descriptor wallet (how?)
+    #       this would help to have repeatable tests, generating always the same seeds
+
+    get_rpc().createwallet(wallet_name=wallet_name, descriptors=True)
+    wallet_rpc = get_wallet_rpc(wallet_name)
+
+    descriptor: str = next(filter(lambda d: d["desc"].startswith(
+        "pkh"), wallet_rpc.listdescriptors()["descriptors"]))["desc"]
+    core_xpub_orig = descriptor[descriptor.index("(")+1: descriptor.index("/0/*")]
+
+    return wallet_name, core_xpub_orig
+
+
+def generate_blocks(n):
+    return get_rpc().generatetoaddress(n, btc_addr)
+
+
+def testnet_to_regtest_addr(addr: str) -> str:
+    """Convenience function to reencode addresses from testnet format to regtest one (bech32 prefix is different)"""
+    hrp, data, spec = segwit_addr.bech32_decode(addr)
+    if hrp is None:
+        return addr  # bech32m decoding failed; either legacy/unknown address type, or invalid address
+    if (hrp != "tb"):
+        raise ValueError("Not a valid testnet bech32m string")
+    return segwit_addr.bech32_encode("bcrt", data, spec)
--- tests/test_e2e_multisig.py
+++ tests/test_e2e_multisig.py
@@ -1,26 +1,105 @@
+import pytest
+
+from typing import Dict
 import hmac
+import json
 from hashlib import sha256
+from decimal import Decimal
+
+from bip32 import BIP32
 
 from bitcoin_client.ledger_bitcoin import Client, MultisigWallet, AddressType
+from bitcoin_client.ledger_bitcoin.psbt import PSBT
+from bitcoin_client.ledger_bitcoin._script import is_p2sh, is_p2wsh, parse_multisig
 
-from test_utils import automation
+from test_utils import SpeculosGlobals
+
+from speculos.client import SpeculosClient
+
+from .conftest import create_new_wallet, generate_blocks, get_unique_wallet_name, get_wallet_rpc, testnet_to_regtest_addr as T
 
 
-@automation("automations/register_wallet_accept.json")
-def test_register_and_get_address(client: Client, speculos_globals):
-    # test for a native segwit wallet (bech32 address)
+def extract_our_pubkeys(psbt: PSBT, master_fp: bytes) -> Dict[int, bytes]:
+    # It only works for standard wallets and simple multisig; won't generalize to miniscript
+    # based on code in bitcoin-core/HWI
 
+    psbt2 = PSBT()
+    psbt2.deserialize(psbt.serialize())
+    if (psbt2.version is None or psbt.version == 0):
+        psbt2.to_psbt_v2()
+
+    pubkeys: Dict[int, bytes] = {}
+    for input_num, psbt_in in enumerate(psbt2.inputs):
+        utxo = None
+        scriptcode = b""
+        if psbt_in.witness_utxo:
+            utxo = psbt_in.witness_utxo
+        if psbt_in.non_witness_utxo:
+            print(psbt_in.output_index)
+            assert psbt_in.output_index is not None
+            utxo = psbt_in.non_witness_utxo.vout[psbt_in.output_index]
+        if utxo is None:
+            continue
+        scriptcode = utxo.scriptPubKey
+
+        if is_p2sh(scriptcode):
+            if len(psbt_in.redeem_script) == 0:
+                continue
+            scriptcode = psbt_in.redeem_script
+
+        # Check if P2WSH
+        if is_p2wsh(scriptcode):
+            if len(psbt_in.witness_script) == 0:
+                continue
+            scriptcode = psbt_in.witness_script
+
+        multisig = parse_multisig(scriptcode)
+        if multisig is not None:
+            _, ms_pubkeys = multisig
+
+            our_keys = 0
+            for pub in ms_pubkeys:
+                if pub in psbt_in.hd_keypaths:
+                    pk_origin = psbt_in.hd_keypaths[pub]
+                    if pk_origin.fingerprint == master_fp:
+                        our_keys += 1
+                        pubkeys[input_num] = pub
+
+            if our_keys > 1:
+                raise ValueError("Cannot have more than 1 internal key in a supported wallet")
+        else:
+            for key, origin in psbt_in.hd_keypaths.items():
+                if origin.fingerprint == master_fp:
+                    pubkeys[input_num] = key
+
+            for key, (_, origin) in psbt_in.tap_bip32_paths.items():
+                if key == psbt_in.tap_internal_key and origin.fingerprint == master_fp:
+                    pubkeys[input_num] = key
+    return pubkeys
+
+
+def test_e2e_multisig(rpc, rpc_test_wallet, client: Client, speculos_globals: SpeculosGlobals, is_speculos: bool, comm: SpeculosClient):
+    if not is_speculos:
+        pytest.skip("Requires speculos")
+
+    wallet_name, core_xpub_orig = create_new_wallet()
+    wallet_rpc = get_wallet_rpc(wallet_name)
+
+    bip32 = BIP32.from_seed(speculos_globals.seed, network="test")
+    internal_xpub = bip32.get_xpub_from_path("m/48'/1'/0'/2'")
     wallet = MultisigWallet(
         name="Cold storage",
         address_type=AddressType.WIT,
         threshold=2,
         keys_info=[
-            "[76223a6e/48'/1'/0'/2']tpubDE7NQymr4AFtewpAsWtnreyq9ghkzQBXpCZjWLFVRAvnbf7vya2eMTvT2fPapNqL8SuVvLQdbUbMfWLVDCZKnsEBqp6UK93QEzL8Ck23AwF/**",
-            "[f5acc2fd/48'/1'/0'/2']tpubDFAqEGNyad35aBCKUAXbQGDjdVhNueno5ZZVEn3sQbW5ci457gLR7HyTmHBg93oourBssgUxuWz1jX5uhc1qaqFo9VsybY1J5FuedLfm4dK/**",
+            f"{core_xpub_orig}/**",
+            f"[{'{0:08x}'.format(speculos_globals.master_key_fingerprint)}/48'/1'/0'/2']{internal_xpub}/**",
         ],
     )
 
+    comm.set_automation_rules(json.load(open("automations/register_wallet_accept.json")))
     wallet_id, wallet_hmac = client.register_wallet(wallet)
+    comm.set_automation_rules({"version": 1, "rules": []})
 
     assert wallet_id == wallet.id
 
@@ -29,6 +108,88 @@ def test_register_and_get_address(client: Client, speculos_globals):
         wallet_hmac,
     )
 
-    res = client.get_wallet_address(wallet, wallet_hmac, 0, 3, False)
+    address_hww = client.get_wallet_address(wallet, wallet_hmac, 0, 3, False)
 
-    assert res == "tb1qwuxulrpu5d02eag4tphxhamaa24s8sk8d5s7kw340cesr0wf87csks3c9a"
+    # ==> verify the address matches what bitcoin-core computes
+    receive_descriptor = wallet.get_descriptor(change=False)
+    receive_descriptor_info = rpc.getdescriptorinfo(receive_descriptor)
+    # bitcoin-core adds the checksum, and requires it for other calls
+    receive_descriptor_chk = receive_descriptor_info["descriptor"]
+    address_core = rpc.deriveaddresses(receive_descriptor_chk, [3, 3])[0]
+
+    assert T(address_hww) == address_core
+
+    # also get the change descriptor for later
+    change_descriptor = wallet.get_descriptor(change=True)
+    change_descriptor_info = rpc.getdescriptorinfo(change_descriptor)
+    change_descriptor_chk = change_descriptor_info["descriptor"]
+
+    # ==> import wallet in bitcoin-core
+
+    multisig_wallet_name = get_unique_wallet_name()
+    rpc.createwallet(
+        wallet_name=multisig_wallet_name,
+        disable_private_keys=True,
+        descriptors=True,
+    )
+    multisig_rpc = get_wallet_rpc(multisig_wallet_name)
+    multisig_rpc.importdescriptors([{
+        "desc": receive_descriptor_chk,
+        "active": True,
+        "internal": False,
+        "timestamp": "now"
+    }, {
+        "desc": change_descriptor_chk,
+        "active": True,
+        "internal": True,
+        "timestamp": "now"
+    }])
+
+    # ==> fund the multisig wallet and get prevout info
+
+    rpc_test_wallet.sendtoaddress(T(address_hww), "0.1")
+    generate_blocks(1)
+
+    assert multisig_rpc.getwalletinfo()["balance"] == Decimal("0.1")
+
+    # ==> prepare a psbt spending from the multisig wallet
+
+    out_address = rpc_test_wallet.getnewaddress()
+
+    result = multisig_rpc.walletcreatefundedpsbt(outputs={
+        out_address: Decimal("0.01")
+    })
+
+    psbt_b64 = result["psbt"]
+
+    # ==> sign it with the hww
+
+    psbt = PSBT()
+    psbt.deserialize(psbt_b64)
+
+    pubkeys = extract_our_pubkeys(psbt, speculos_globals.master_key_fingerprint.to_bytes(4, byteorder="big"))
+
+    comm.set_automation_rules(json.load(open("automations/sign_with_wallet_accept.json")))
+    hww_sigs = client.sign_psbt(psbt, wallet, wallet_hmac)
+    comm.set_automation_rules({"version": 1, "rules": []})
+
+    assert len(hww_sigs) == len(pubkeys)  # should be true as long as all inputs are internal
+
+    for i, sig in hww_sigs.items():
+        pubkey = pubkeys[i]
+        psbt.inputs[i].partial_sigs[pubkey] = sig
+
+    signed_psbt_hww_b64 = psbt.serialize()
+
+    # ==> sign it with bitcoin-core
+
+    signed_psbt_core_b64 = wallet_rpc.walletprocesspsbt(psbt_b64)["psbt"]
+
+    # ==> finalize the psbt, extract tx and broadcast
+    combined_psbt = rpc.combinepsbt([signed_psbt_hww_b64, signed_psbt_core_b64])
+    result = rpc.finalizepsbt(combined_psbt)
+    rawtx = result["hex"]
+    assert result["complete"] == True
+
+    # make sure the transaction is valid by broadcasting it (would fail if rejected)
+    rpc.sendrawtransaction(rawtx)
