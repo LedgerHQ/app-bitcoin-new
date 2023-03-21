cffb0fc8f4d0e2cde7b1029b3c5d835438afe85f
Salvatore Ingala <6681844+bigspider@users.noreply.github.com>
2023-03-21 11:31:45 +0100

    Test that the app correctly responds with WRONG_P1P2 if p2 > 1

---
+++ tests/test_protocol.py
@@ -0,0 +1,18 @@
+import pytest
+
+from bitcoin_client.ledger_bitcoin import Client
+from bitcoin_client.ledger_bitcoin.client_base import ApduException
+from bitcoin_client.ledger_bitcoin.command_builder import BitcoinCommandBuilder, BitcoinInsType, CURRENT_PROTOCOL_VERSION
+
+
+def test_p2_too_high(client: Client):
+    # Tests that sending a p2 > CURRENT_PROTOCOL_VERSION fails with 0x6a86 (WRONG_P1P2)
+    with pytest.raises(ApduException, match="Exception: invalid status 0x6a86"):
+        # We can't use the client to send this apdu, so we use raw transport
+        client.transport_client.apdu_exchange(
+            cla=BitcoinCommandBuilder.CLA_BITCOIN,
+            ins=BitcoinInsType.GET_MASTER_FINGERPRINT,
+            p1=0,
+            p2=CURRENT_PROTOCOL_VERSION + 1,
+            data=b''
+        )
