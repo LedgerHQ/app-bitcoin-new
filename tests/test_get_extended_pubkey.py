3c9779e658482ab5400ed65763c22b245e83f1ff
Sarah GLINER <sarah.gliner@ledger.fr>
2023-08-23 15:29:32 +0200

    Tests: temporary fix for broken LNX OCR

--- tests/test_get_extended_pubkey.py
+++ tests/test_get_extended_pubkey.py
@@ -165,7 +165,12 @@ def test_get_extended_pubkey_non_standard_reject_early(client: Client, comm: Spe
         comm.press_and_release("right")
         comm.wait_for_text_event("Confirm public key")
         comm.press_and_release("right")
-        comm.wait_for_text_event("111'/222'/333'")
+        # Temporary fix for broken OCR
+        if (model == "nanox"):
+            comm.wait_for_text_event("111-/222-/333-")
+        else:
+            comm.wait_for_text_event("111'/222'/333'")
+
         comm.press_and_release("right")
         comm.wait_for_text_event("not sure")  # second line of "Reject if you're not sure"
         comm.press_and_release("both")
