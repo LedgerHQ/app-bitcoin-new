19d6c5ec5893c20a881cd778dfba6cecd5888464
Salvatore Ingala <6681844+bigspider@users.noreply.github.com>
2024-06-21 10:52:33 +0200

    Add payin_extra_id to G_swap_state; process destination_address_extra_id during copy_transaction_parameters

--- src/swap/swap_globals.h
+++ src/swap/swap_globals.h
@@ -2,6 +2,11 @@
 
 #include <stdint.h>
 
+enum {
+    SWAP_MODE_STANDARD = 0,
+    SWAP_MODE_CROSSCHAIN = 1,
+};
+
 typedef struct swap_globals_s {
     uint64_t amount;
     uint64_t fees;
@@ -9,6 +14,8 @@ typedef struct swap_globals_s {
     /*Is swap mode*/
     unsigned char called_from_swap;
     unsigned char should_exit;
+    unsigned char mode;
+    uint8_t payin_extra_id[1 + 32];
 } swap_globals_t;
 
 extern swap_globals_t G_swap_state;
